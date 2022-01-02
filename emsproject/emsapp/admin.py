c9560aca926bbfd9003828b2269856bf47dd3174
mowuj <mowuj92@gmail.com>
2022-01-03 00:21:51 +0600

    first commit

---
+++ emsproject/emsapp/admin.py
@@ -0,0 +1,50 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+class DistrictAdmin(admin.ModelAdmin):
+    model=District
+    list_display = ['name','division']
+
+class EmployeeAdmin(admin.ModelAdmin):
+    model=Employee
+    list_display = ['username',
+                    'department',
+                    'district','full_name',
+                    'email','phone'
+                    ,'father_name',
+                    'mother_name']
+
+
+class LeaveAdmin(admin.ModelAdmin):
+    model=District
+    list_display = ['user',
+                    'cause_of_leave',
+                    'start_date',
+                    'end_date',
+                    'check_status',
+                    'approve_status']
+    list_editable = ['check_status','approve_status']
+
+class HolidayAdmin(admin.ModelAdmin):
+    model=District
+    list_display = ['Description','start_date','Description']
+
+class DailyTaskAdmin(admin.ModelAdmin):
+    model=DailyTask 
+    list_display = ['user',
+                    'title',
+                    'description',
+                    'working_status',
+                    'do_status',
+                    'done_status']
+
+admin.site.register(Department)
+admin.site.register(Division)
+admin.site.register(Post)
+admin.site.register(Meeting)
+admin.site.register(District,DistrictAdmin)
+admin.site.register(Leave,LeaveAdmin)
+admin.site.register(Employee,EmployeeAdmin)
+admin.site.register(Holiday,HolidayAdmin)
+admin.site.register(DailyTask,DailyTaskAdmin)
---
+++ emsproject/emsapp/filters.py
@@ -0,0 +1,26 @@
+
+import django_filters
+from django_filters import DateFilter,CharFilter
+from .models import*
+
+class EmployeeFilter(django_filters.FilterSet):
+    # start_date = DateFilter(field_name="date",lookup_expr='gte')
+    # end_date = DateFilter(field_name="date",lookup_expr='lte')
+    
+    class Meta:
+        model = Employee
+        fields = '__all__'
+        exclude = ['full_name',
+                    'email',
+                    'district',
+                    'phone',
+                    'nid',
+                    'ssc',
+                    'hsc',
+                    'honors',
+                    'masters',
+                    'selary',
+                    'email',
+                    'image',
+                    'father_name',
+                    'mother_name']
